acb3e5f0965c864f1f6362ad47731765338a4fc1
Alvaro Mongil <hello@alvaromongil.com>
2021-12-28 17:17:15 +0100

    dirs

---
+++ run_mlflow_project.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import warnings
+
+import mlflow
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+
+    project_uri = os.path.join(os.path.dirname(os.path.abspath(__file__)), "thesis-mlflow-project")
+    params = {"alpha": sys.argv[1], "l1_ratio": sys.argv[2]}
+
+    mlflow.set_tracking_uri(sys.argv[3])
+    mlflow.run(
+        project_uri,
+        parameters=params,
+        backend="kubernetes",
+        backend_config="thesis-mlflow-project/kubernetes_config.json",
+    )
